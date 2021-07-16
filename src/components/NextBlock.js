683fccba04cc533a6364fc35978993f972e66b37
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-15 20:36:51 -0700

    completed tetris game :)

--- src/components/NextBlock.js
+++ src/components/NextBlock.js
@@ -7,15 +7,12 @@ import { shapes } from '../utils/index'
 // Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
   const nextShape = useSelector((state) => state.game.nextShape)
-
   const box = shapes[nextShape][0] // Get the first rotation
-  
-  // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
   
   // Map the block to the grid
   const grid = box.map((rowArray, row) => {
       return rowArray.map((square, col) => {
-          return <GridSquare key={`${row}${col}`} color={square} />
+        return <GridSquare key={`${row}${col}`} color={square === 0 ? 0 : nextShape} />
       })
   })
 
